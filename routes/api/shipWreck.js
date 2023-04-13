746e2fe6f97fbb3b3be493772a6418633cbaa3ba
AnVin007 <128915168+AnVin007@users.noreply.github.com>
2023-04-13 07:15:25 -0400

    New Updates
    -Added new model Person (for Register and Login)
    -Added some new front end .hbs pages
    -Added JWT Strategy folder with file (prof implementation) - need to update
    -Added style folder with style.css
    (need to update styles)

--- routes/api/shipWreck.js
+++ routes/api/shipWreck.js
@@ -6,6 +6,14 @@ const ShipWreck = require("../../models/ShipWreck");
 
 router.get("/", (req, res) => res.send("ShipWreck related routes"));
 
+/* -------------------------------------------------------- FRONT END FORMS ----------------------------------------------- */
+router.get('/addForm', function (req, res) {
+  res.render('addForm')
+})
+
+/* -------------------------------------------------------- SHOW DATA ----------------------------------------------- */
+//Show all data based on number of instances per page. 
+//Filtered(optionaly) by depth
 router.get("/data", async (req, res) => {
   const page = parseInt(req.query.page);
   const perPage = parseInt(req.query.perPage);
@@ -24,6 +32,7 @@ router.get("/data", async (req, res) => {
   }
 });
 
+//Show particular shipwreck based on it's _id in collection
 router.get("/data/:_id", (req, res) => {
   x = new ObjectId(req.params._id);
   ShipWreck.findOne({ _id: x })
@@ -39,6 +48,7 @@ router.get("/data/:_id", (req, res) => {
     });
 });
 
+/* -------------------------------------------------------- ADD DATA ----------------------------------------------- */
 router.post("/add", (req, res) => {
   const newShipWreck = new ShipWreck({
     feature_type: req.body.feature_type,
@@ -51,10 +61,19 @@ router.post("/add", (req, res) => {
 
   newShipWreck
     .save()
-    .then((shipwrk) => res.send(shipwrk))
+    .then(
+      // (shipwrk) => res.send(shipwrk)   <-- We dont want to show data about currently added shipwreck. We want to get back to home page
+
+      //Send Feedback Message at Home Page
+      res.render('index',
+      {
+          data: "Registered Successfully"
+      })
+    )
     .catch((err) => console.log(err));
 });
 
+/* -------------------------------------------------------- UPDATE DATA ----------------------------------------------- */
 router.put("/data/:_id", (req, res) => {
   x = new ObjectId(req.params._id);
   ShipWreck.updateOne(
@@ -76,7 +95,7 @@ router.put("/data/:_id", (req, res) => {
         return res.status(404).send("Data not found");
       } else {
         console.log(data);
-        res.status(201).send("Shipwreck Deleted.");
+        res.status(201).send("Shipwreck Updated.");
       }
     })
     .catch((err) => {
@@ -84,6 +103,7 @@ router.put("/data/:_id", (req, res) => {
     });
 });
 
+/* -------------------------------------------------------- DELETE DATA ----------------------------------------------- */
 router.delete("/data/:_id", (req, res) => {
   x = new ObjectId(req.params._id);
   ShipWreck.deleteOne({ _id: x })
