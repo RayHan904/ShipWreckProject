eae21f5dffb0ac0d5d8e728cb741c1435efb3b82
Sawankkhanna <97986867+Sawankkhanna@users.noreply.github.com>
2023-04-13 21:07:34 -0400

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 module.exports = {
   mongoDBUrl:
-    "mongodb+srv://Rehan:Rehan123@cluster0.qwj82f0.mongodb.net/sample_geospatial?retryWrites=true&w=majority",
+    "mongodb+srv://" + process.env.USER + ":" + process.env.PASSWORD + "@cluster0.qwj82f0.mongodb.net/sample_geospatial?retryWrites=true&w=majority",
   secret: "verySecretCode",
 };
